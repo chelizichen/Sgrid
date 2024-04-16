d3c26c68e5df40c88824377d3fc66f0ee9417f0b
chelizichen <1347290221@qq.com>
2024-04-16 16:47:32 +0800

    feat

--- src/public/lock/index.go
+++ src/public/lock/index.go
@@ -32,3 +32,50 @@ func (s *SgridLock) Lock() bool {
 func (s *SgridLock) Unlock() {
 	s.targetLock.Store(false)
 }
+
+const SyncSateMapMaxFaileTime = 4
+
+type SyncSafetyMap[K comparable, V comparable] struct {
+	value      map[K]V
+	targetLock atomic.Bool
+	backoff    int8
+}
+
+func (s *SyncSafetyMap[K, V]) Set(key K, Value V) bool {
+	return s.lock(func() {
+		s.value[key] = Value
+	})
+}
+
+func (s *SyncSafetyMap[K, V]) Delete(key K) bool {
+	return s.lock(func() {
+		delete(s.value, key)
+	})
+}
+
+func (s *SyncSafetyMap[K, V]) Get(key K) V {
+	return s.value[key]
+}
+
+func (s *SyncSafetyMap[K, V]) lock(event func()) bool {
+	b := s.targetLock.Swap(true)
+	if b {
+		if s.backoff <= SyncSateMapMaxFaileTime {
+			s.backoff <<= 1
+		}
+		defer func() {
+			for i := 0; i < int(s.backoff); i++ {
+				runtime.Gosched()
+			}
+		}()
+		return false
+	}
+	defer s.unlock()
+	event()
+	s.backoff = 0
+	return true
+}
+
+func (s *SyncSafetyMap[K, V]) unlock() {
+	s.targetLock.Store(false)
+}
