5eb3becaa04fae6a4a3afb13379f66184d3645e8
chelizichen <1347290221@qq.com>
2024-11-04 23:47:19 +0800

    ui 优化

--- client/src/views/devops/aside.vue
+++ client/src/views/devops/aside.vue
@@ -1,25 +1,15 @@
 <script lang="ts">
-// devops component
-// 1.选择服务组 ｜ 创建
-// 2.创建服务
-// 3.选择节点
-// 4.添加至服务网格
-// 5.选择端口
 export default {
-  name: 'devops-component'
-}
+  name: "devops-component",
+};
 </script>
 <template>
   <div>
     <el-container>
-      <el-aside style="width: 200px; height: 100vh">
-        <el-menu
-          class="el-menu-vertical-demo"
-          active-text-color="var(--sgrid-primary-choose-color)"
-        >
+      <el-aside class="app h-screen w-48">
+        <el-menu class="border-none" active-text-color="var(--sgrid-primay-hover-color)">
           <el-sub-menu v-for="(item, index) in menus" :index="String(index)" :key="index">
             <template #title>
-              <el-icon><grid /></el-icon>
               <span>{{ item.title }}</span>
             </template>
             <el-menu-item
@@ -27,6 +17,7 @@ export default {
               :key="s_index"
               :index="index + '_' + s_index"
               @click="push(item, s_item)"
+              class="h-8"
               >{{ s_item.title }}</el-menu-item
             >
           </el-sub-menu>
@@ -40,21 +31,25 @@ export default {
 </template>
 
 <script setup lang="ts">
-import { useUserStore } from '@/stores/counter'
-import { computed } from 'vue'
-import { useRouter } from 'vue-router'
-const router = useRouter()
-const userStore = useUserStore()
+import { useUserStore } from "@/stores/counter";
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const userStore = useUserStore();
 const menus = computed(() => {
-  console.log('userStore.menus', userStore.menus)
-  return userStore.menus
-})
-const base_path = '/devops/'
+  console.log("userStore.menus", userStore.menus);
+  return userStore.menus;
+});
+const base_path = "/devops/";
 function push(item: any, s_item: any) {
-  const path = base_path + item.path + '/' + s_item.path
-  router.push(path)
-  console.log(item, s_item)
+  const path = base_path + item.path + "/" + s_item.path;
+  router.push(path);
+  console.log(item, s_item);
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.app >>> .el-sub-menu__title {
+  height: 44px;
+}
+</style>
